014efa5271ed53b097291033fb6adf374ec5e0ad
aya_ml <91435365+aya-ml@users.noreply.github.com>
2026-02-05 16:08:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,16 @@
-// Имя кэша
-const CACHE_NAME = 'notes-pwa-v1';
+const CACHE_NAME = 'piano-notes-v1';
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
-// Файлы для кэширования
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// Установка Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Кэшируем файлы для офлайн работы');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Активация
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Перехват запросов
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Если файл есть в кэше, возвращаем его
-        if (response) {
-          return response;
-        }
-        // Иначе загружаем из сети
-        return fetch(event.request);
-      })
+      .then(response => response || fetch(event.request))
   );
 });
